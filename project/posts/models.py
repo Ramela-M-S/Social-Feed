4d569b925138498f849566ec38371bd68539d8d0
Nawin M S <ramelams07@gmail.com>
2025-12-17 15:20:43 +0530

    Changed the UI

--- project/posts/models.py
+++ project/posts/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Post(models.Model):
-    user    = models.OneToOneField(User, on_delete=models.CASCADE, related_name="posts")
+    user    = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
     title   = models.CharField(
         max_length=200
     )
