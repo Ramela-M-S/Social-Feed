a0162c97917c0e677d0642ec7b338b0c816eada0
Nawin M S <ramelams07@gmail.com>
2025-12-17 22:35:00 +0530

    Add build script to run migrations on Render

--- project/urls.py
+++ project/urls.py
@@ -29,6 +29,6 @@ urlpatterns = [
     path('', include('feed.urls')),
     path('admin/', admin.site.urls),
 ]
-if settings.DEBUG== False:
+if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
