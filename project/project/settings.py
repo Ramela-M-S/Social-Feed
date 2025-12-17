5249d807847bec6c687591707102fbaa7a0f985b
Nawin M S <ramelams07@gmail.com>
2025-12-17 16:37:18 +0530

    Final commit for Render

--- project/project/settings.py
+++ project/project/settings.py
@@ -80,12 +80,20 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL')  # your existing DB
-    )
-}
+DATABASE_URL = os.environ.get("DATABASE_URL")  # Render will provide this
 
+if DATABASE_URL:
+    DATABASES = {
+        "default": dj_database_url.parse(DATABASE_URL)
+    }
+else:
+    # Local development database
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
