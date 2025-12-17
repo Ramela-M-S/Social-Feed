9447864795e1e8e08dde6aa1a386377523fefc75
Nawin M S <ramelams07@gmail.com>
2025-12-17 20:50:23 +0530

    Initial commit: Django social feed project

--- project/settings.py
+++ project/settings.py
@@ -29,8 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'fallback-secret-key')
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 # Allowed Hosts
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
-
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
 # Application definition
 
 INSTALLED_APPS = [
