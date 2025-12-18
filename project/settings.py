04d95c0df874888361fd486f597e623e335a4387
Nawin M S <ramelams07@gmail.com>
2025-12-18 05:53:17 +0530

    Cloudinary added

--- project/settings.py
+++ project/settings.py
@@ -141,6 +141,10 @@ CRISPY_TEMPLATE_PACK          = "bootstrap4"
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/menu/'
 LOGOUT_REDIRECT_URL = '/'
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
+}
